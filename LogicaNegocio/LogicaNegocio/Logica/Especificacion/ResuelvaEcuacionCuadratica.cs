341af2e1012c559060bcb2c656ea3ca33091d6b3
JorgeGuillen <35737009+JorgeGuillen@users.noreply.github.com>
2018-10-08 11:56:28 -0600

    Ecuaciones Cuadraticas

---
+++ LogicaNegocio/LogicaNegocio/Logica/Especificacion/ResuelvaEcuacionCuadratica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogicaNegocio.Logica.Especificacion
+{
+    public class ResuelvaEcuacionCuadratica
+    {
+        public ResultadoEcuacionCuadratica ResolverEcuacionCuadratica(float a, float b, float c)
+        {
+            var laAccion = new Logica.Accion.ResolverEcuacionCuadratica();
+            ResultadoEcuacionCuadratica elResultado = laAccion.ResuelvaEcuacionCuadratica(a, b, c);
+            return elResultado;
+        }
+
+
+    }
+}
